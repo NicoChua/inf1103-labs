f13ca9f0854b4c27815689cf610b4f9f34b26476
NicoChua <nicochua526@gmail.com>
2026-09-11 15:17:22 +0800

    Smart Inventory Auditor Base

--- Lab-02/auditor.py
+++ Lab-02/auditor.py
@@ -1,1 +1,13 @@
-print("Hello World")
+inventory = 0
+failedEntry = 0
+
+menuOpen = True
+while menuOpen == True:
+    stockQty = input("Enter Stock Quantity: ")
+    if stockQty == "Quit":
+        menuOpen = False
+    elif stockQty.isdigit() == True:
+        inventory += int(stockQty)
+    else:
+        print("Please input a number")
+        failedEntry += 1
