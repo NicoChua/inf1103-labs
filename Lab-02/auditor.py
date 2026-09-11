04503fb4ed80cb62216a074019a2219c4c9c5efd
NicoChua <nicochua526@gmail.com>
2026-09-11 15:17:56 +0800

    Trigger Overstock Alert

--- Lab-02/auditor.py
+++ Lab-02/auditor.py
@@ -3,6 +3,9 @@ failedEntry = 0
 
 menuOpen = True
 while menuOpen == True:
+    if inventory > 500:
+        print("Inventory Exceeded 500 units")
+        break
     stockQty = input("Enter Stock Quantity: ")
     if stockQty == "Quit":
         menuOpen = False
