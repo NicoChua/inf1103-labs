3fe069ac354b2765329617f0057760b98ccb52ba
NicoChua <2603065@sit.singaporetech.edu.sg>
2026-09-18 19:11:41 +0800

    Added Valid Input Function and Main Function

--- Lab-03/modular_auditor.py
+++ Lab-03/modular_auditor.py
@@ -1,19 +1,29 @@
-inventory = 0
-failedEntry = 0
+# Global Constants
+MAX_CAPACITY = 500
+TAX_RATE = 0.1
+FAILED_ENTRY = 0    
 
-menuOpen = True
-while menuOpen == True:
-    if inventory > 500:
-        print("Inventory Exceeded 500 units")
-        break
-    stockQty = input("Enter Stock Quantity: ")
-    if stockQty == "Quit":
-        menuOpen = False
-    elif stockQty.isdigit() == True:
-        inventory += int(stockQty)
-    else:
+def main():
+    inventory = 0
+    tax_amount = 0
+    exit_program = False
+    global MAX_CAPACITY
+
+    while not exit_program:
+        stockQty = get_valid_input()
+        if stockQty == "quit":
+            exit_program = True
+
+def get_valid_input():
+    global FAILED_ENTRY
+    user_input = input("Enter Stock Quantity: ")
+    if user_input == "quit":
+        return "quit"
+    elif user_input.isdigit() != True:
         print("Please input a number")
-        failedEntry += 1
-        
-print("Total units processed: " + str(inventory))
-print("Number of Failed/Rejected Entries: " + str(failedEntry))
+        FAILED_ENTRY += 1
+        return None
+    return user_input
+
+if __name__ == "__main__":
+    main()
