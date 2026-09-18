d36978fb75fcdeed025f718f1084d2eff0168187
NicoChua <2603065@sit.singaporetech.edu.sg>
2026-09-18 19:09:20 +0800

    Added Dockerfile

---
+++ Lab-03/modular_auditor.py
@@ -0,0 +1,19 @@
+inventory = 0
+failedEntry = 0
+
+menuOpen = True
+while menuOpen == True:
+    if inventory > 500:
+        print("Inventory Exceeded 500 units")
+        break
+    stockQty = input("Enter Stock Quantity: ")
+    if stockQty == "Quit":
+        menuOpen = False
+    elif stockQty.isdigit() == True:
+        inventory += int(stockQty)
+    else:
+        print("Please input a number")
+        failedEntry += 1
+        
+print("Total units processed: " + str(inventory))
+print("Number of Failed/Rejected Entries: " + str(failedEntry))
